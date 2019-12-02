f6c0f0667f118953fc0a5eb3a3294d30f3a09b1c
Tom Himanen <tom.himanen@gmail.com>
2019-12-02 15:50:42 +0200

    Initial commit

---
+++ Assets/Koodit/PelaajaKoodi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelaajaKoodi : MonoBehaviour
+{
+    public float nopeus;
+    Vector3 suunta;
+    Rigidbody RB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate(){
+        suunta.x = Input.GetAxis("Horizontal");
+        suunta.z = Input.GetAxis("Vertical");
+        RB.AddForce(suunta * nopeus);
+    }
+}
